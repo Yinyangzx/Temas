99bf9d83176c4de37e3da746d86fcf7519a4734f
Yinyangzx <xonoke1@gmail.com>
2026-09-24 21:48:57 -0300

    Update background asset ID in YinYang_Themes.lua

--- YinYang_Themes.lua
+++ YinYang_Themes.lua
@@ -106,7 +106,7 @@ return {
                 ToggleOn   = RGB(52, 199, 89),
             },
             Sound      = DEFAULT_SOUND,
-            Background = "rbxassetid://138004303203419",
+            Background = "rbxassetid://95195702932261",
             Effect     = "Off",
         },
 
